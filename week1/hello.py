fc708ade1b5bad6d8a2e3cb0fc38320c73af6139
BaffourOduro <baffour.oduro007@gmail.com>
2026-07-28 17:55:02 +0000

    Initial commit

---
+++ week1/hello.py
@@ -0,0 +1,19 @@
+"""
+Week 1 · Lab 1.1 — My First Python Script
+=========================================
+Follow the lab on the course dashboard, and write your code here.
+
+Run it with:   python week1/hello.py
+"""
+
+# TODO: print a greeting
+print("Good evening, everyone!")
+
+# TODO: create a variable with your name, and use it in a message
+name = "Shadrack"
+
+# TODO: 
+message = "Good evening, I am "
+
+
+print(message + name )
